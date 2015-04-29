5711feaf760150ee93a90823f7e86b8734f8e332
Justin R. Evans <jurevans@gmail.com>
2015-04-29 10:45:15 -0400

    Update settings.py

--- lpg/settings.py
+++ lpg/settings.py
@@ -29,7 +29,7 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 
 ACCOUNT_ACTIVATION_DAYS = 2
 EMAIL_HOST = 'localhost'
-DEFAULT_FROM_EMAIL = 'no-reply@valetleads.com'
+DEFAULT_FROM_EMAIL = 'no-reply@somedomain.com'
 LOGIN_REDIRECT_URL = '/admin'
 
 #EMAIL_USE_TLS = True
@@ -57,7 +57,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/django/landing_page_generator/media/'
+MEDIA_ROOT = '/path/to/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
